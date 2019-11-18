51f055431c032e7739907a0de8ceaa102d01408a
maxdavid <me@maxdavid.com>
2019-11-18 11:16:16 -0800

    Set up base hook/reducer/context model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import * as reset from './styles/reset.css';
+import * as global from './styles/global.css';
 import './index.css';
+
+import { StateProvider } from './contexts/StateProvider';
+import { initialState } from './reducers/initialState';
+import { rootReducer } from './reducers/rootReducer';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+    ${reset} 
+    ${global}
+`;
+
+const theme = {
+  // primaryColor: 'blue',
+  // secondaryColor: 'green'
+};
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Router>
+      <StateProvider initialState={initialState} reducer={rootReducer}>
+        <App />
+      </StateProvider>
+    </Router>
+  </ThemeProvider>,
+  document.getElementById('root')
+);
